964ed126e7c8dab8f3659b9807c151d4e1c20afa
p-louis <patrick.louis@mailbox.org>
2022-04-26 07:01:04 +0200

    Refactored Coordinate formatting to formatter based

--- dcs-dtc/UI/Aircrafts/FA18/PrePlannedEdit.cs
+++ dcs-dtc/UI/Aircrafts/FA18/PrePlannedEdit.cs
@@ -130,7 +130,7 @@ namespace DTC.UI.Aircrafts.FA18
 						txtWptLatLong.Text = latitude + " " + longitude;
 						txtWptElevation.Text = elevation;
 					}));
-				}, longFormat: true, dmsFormat: true);
+				}, new DmsLongFormatter());
 			}
 			else
 			{

2fb5ec7be53a530b26e2d75be5b1872d34747e70
p-louis <patrick.louis@mailbox.org>
2022-06-21 14:33:00 +0200

    Added Scrollbar to main menu

--- dcs-dtc/UI/CommonPages/AircraftPage.Designer.cs
+++ dcs-dtc/UI/CommonPages/AircraftPage.Designer.cs
@@ -29,40 +29,41 @@ namespace DTC.UI.CommonPages
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pnlMain = new System.Windows.Forms.Panel();
-			this.pnlLeft = new System.Windows.Forms.Panel();
-			this.SuspendLayout();
-			// 
-			// pnlMain
-			// 
-			this.pnlMain.BackColor = System.Drawing.Color.PaleGoldenrod;
-			this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pnlMain.Location = new System.Drawing.Point(182, 0);
-			this.pnlMain.Name = "pnlMain";
-			this.pnlMain.Size = new System.Drawing.Size(564, 684);
-			this.pnlMain.TabIndex = 6;
-			// 
-			// pnlLeft
-			// 
-			this.pnlLeft.BackColor = System.Drawing.Color.DarkKhaki;
-			this.pnlLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
-			this.pnlLeft.Location = new System.Drawing.Point(0, 0);
-			this.pnlLeft.Margin = new System.Windows.Forms.Padding(10);
-			this.pnlLeft.Name = "pnlLeft";
-			this.pnlLeft.Padding = new System.Windows.Forms.Padding(5);
-			this.pnlLeft.Size = new System.Drawing.Size(182, 684);
-			this.pnlLeft.TabIndex = 5;
-			// 
-			// AircraftPage
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.pnlMain);
-			this.Controls.Add(this.pnlLeft);
-			this.Name = "AircraftPage";
-			this.Size = new System.Drawing.Size(746, 684);
-			this.ResumeLayout(false);
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(182, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(564, 684);
+            this.pnlMain.TabIndex = 6;
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.AutoScroll = true;
+            this.pnlLeft.BackColor = System.Drawing.Color.DarkKhaki;
+            this.pnlLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeft.Margin = new System.Windows.Forms.Padding(10);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlLeft.Size = new System.Drawing.Size(182, 684);
+            this.pnlLeft.TabIndex = 5;
+            // 
+            // AircraftPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlLeft);
+            this.Name = "AircraftPage";
+            this.Size = new System.Drawing.Size(746, 684);
+            this.ResumeLayout(false);
 
 		}
 

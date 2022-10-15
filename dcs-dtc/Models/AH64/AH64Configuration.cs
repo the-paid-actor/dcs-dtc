5246e60fd7aab788c350c9cc9f9d098b2eb472c8
PieterL1976 <46246478+PieterL1976@users.noreply.github.com>
2022-10-15 09:16:50 +0200

    Add files via upload
    AH64 Model Files

---
+++ dcs-dtc/Models/AH64/AH64Configuration.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using DTC.Models.Base;
+using DTC.Models.AH64.Waypoints;
+using DTC.Models.AH64.Radios;
+
+namespace DTC.Models.AH64
+{
+    public class AH64Configuration : IConfiguration
+    {
+        public WaypointSystem Waypoints = new WaypointSystem();
+        public RadioSystem Radios = new RadioSystem();
+        public string ToJson()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+
+        public string ToCompressedString()
+        {
+            var json = ToJson();
+            return StringCompressor.CompressString(json);
+        }
+        public static AH64Configuration FromJson(string s)
+        {
+            try
+            {
+                var cfg = JsonConvert.DeserializeObject<AH64Configuration>(s);
+                cfg.AfterLoadFromJson();
+                return cfg;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public void AfterLoadFromJson()
+        {
+
+        }
+        public static AH64Configuration FromCompressedString(string s)
+        {
+            try
+            {
+                var json = StringCompressor.DecompressString(s);
+                var cfg = FromJson(json);
+                return cfg;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public AH64Configuration Clone()
+        {
+            var json = ToJson();
+            var cfg = FromJson(json);
+            return cfg;
+        }
+
+        public void CopyConfiguration(AH64Configuration cfg)
+        {
+            if (cfg.Waypoints != null)
+            {
+                Waypoints = cfg.Waypoints;
+            }
+            if (cfg.Radios != null)
+            {
+                Radios = cfg.Radios;
+            }
+        }
+        IConfiguration IConfiguration.Clone()
+        {
+            return Clone();
+        }
+    }
+}

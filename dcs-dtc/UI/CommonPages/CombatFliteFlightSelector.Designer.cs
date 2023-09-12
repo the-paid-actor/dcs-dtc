aeb774f426790faa5ebd5680884a617be7e6b98e
pikeheadsim <hakeheadsim@gmail.com>
2023-09-12 20:39:05 +0200

    [UI] Flight selector

--- dcs-dtc/UI/CommonPages/CombatFliteFlightSelector.Designer.cs
+++ dcs-dtc/UI/CommonPages/CombatFliteFlightSelector.Designer.cs
@@ -1,115 +1,128 @@
 ﻿namespace DTC.UI.CommonPages
 {
-	partial class CombatFliteFlightSelector
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class CombatFliteFlightSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.comboBoxFlight = new System.Windows.Forms.ComboBox();
-			this.dtcLabel1 = new DTC.UI.Base.Controls.DTCLabel();
-			this.btnCancel = new DTC.UI.Base.Controls.DTCButton();
-			this.btnOK = new DTC.UI.Base.Controls.DTCButton();
-			this.SuspendLayout();
-			// 
-			// comboBoxFlight
-			// 
-			this.comboBoxFlight.Location = new System.Drawing.Point(104, 24);
-			this.comboBoxFlight.Name = "comboBoxFlight";
-			this.comboBoxFlight.Size = new System.Drawing.Size(459, 28);
-			this.comboBoxFlight.TabIndex = 7;
-			this.comboBoxFlight.SelectedIndexChanged += new System.EventHandler(this.comboBoxFlight_SelectedIndexChanged);
-			// 
-			// dtcLabel1
-			// 
-			this.dtcLabel1.AutoSize = true;
-			this.dtcLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-			this.dtcLabel1.Location = new System.Drawing.Point(22, 23);
-			this.dtcLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.dtcLabel1.Name = "dtcLabel1";
-			this.dtcLabel1.Size = new System.Drawing.Size(65, 25);
-			this.dtcLabel1.TabIndex = 10;
-			this.dtcLabel1.Text = "Flight:";
-			// 
-			// btnCancel
-			// 
-			this.btnCancel.BackColor = System.Drawing.Color.DarkKhaki;
-			this.btnCancel.FlatAppearance.BorderSize = 0;
-			this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-			this.btnCancel.Location = new System.Drawing.Point(338, 70);
-			this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(225, 62);
-			this.btnCancel.TabIndex = 8;
-			this.btnCancel.Text = "Cancel";
-			this.btnCancel.UseVisualStyleBackColor = false;
-			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-			// 
-			// btnOK
-			// 
-			this.btnOK.BackColor = System.Drawing.Color.DarkKhaki;
-			this.btnOK.FlatAppearance.BorderSize = 0;
-			this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-			this.btnOK.Location = new System.Drawing.Point(104, 70);
-			this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.btnOK.Name = "btnOK";
-			this.btnOK.Size = new System.Drawing.Size(225, 62);
-			this.btnOK.TabIndex = 9;
-			this.btnOK.Text = "OK";
-			this.btnOK.UseVisualStyleBackColor = false;
-			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
-			// 
-			// CombatFliteFlightSelector
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.PaleGoldenrod;
-			this.ClientSize = new System.Drawing.Size(585, 155);
-			this.ControlBox = false;
-			this.Controls.Add(this.dtcLabel1);
-			this.Controls.Add(this.btnCancel);
-			this.Controls.Add(this.btnOK);
-			this.Controls.Add(this.comboBoxFlight);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "CombatFliteFlightSelector";
-			this.ShowIcon = false;
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "CombatFliteFlightSelector";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBoxFlight = new ComboBox();
+            btnCancel = new Base.Controls.DTCButton();
+            btnOK = new Base.Controls.DTCButton();
+            Group = new Base.Controls.DTCGroupBox();
+            Group.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBoxFlight
+            // 
+            comboBoxFlight.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBoxFlight.Location = new Point(31, 29);
+            comboBoxFlight.Name = "comboBoxFlight";
+            comboBoxFlight.Size = new Size(408, 24);
+            comboBoxFlight.TabIndex = 7;
+            comboBoxFlight.SelectedIndexChanged += comboBoxFlight_SelectedIndexChanged;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = Color.DarkKhaki;
+            btnCancel.FlatAppearance.BorderColor = Color.Black;
+            btnCancel.FlatAppearance.BorderSize = 0;
+            btnCancel.FlatStyle = FlatStyle.Flat;
+            btnCancel.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCancel.Location = new Point(270, 66);
+            btnCancel.Margin = new Padding(4);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(148, 35);
+            btnCancel.TabIndex = 8;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnOK
+            // 
+            btnOK.BackColor = Color.DarkKhaki;
+            btnOK.FlatAppearance.BorderSize = 0;
+            btnOK.FlatStyle = FlatStyle.Flat;
+            btnOK.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOK.Location = new Point(65, 66);
+            btnOK.Margin = new Padding(4);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(148, 35);
+            btnOK.TabIndex = 9;
+            btnOK.Text = "OK";
+            btnOK.UseVisualStyleBackColor = false;
+            btnOK.Click += btnOK_Click;
+            // 
+            // Group
+            // 
+            Group.BackColor = Color.PaleGoldenrod;
+            Group.BorderColor = Color.Black;
+            Group.BorderRadius = 2;
+            Group.BorderWidth = 2;
+            Group.Controls.Add(btnOK);
+            Group.Controls.Add(comboBoxFlight);
+            Group.Controls.Add(btnCancel);
+            Group.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            Group.ForeColor = Color.Black;
+            Group.LabelIndent = 10;
+            Group.Location = new Point(3, -1);
+            Group.Margin = new Padding(0);
+            Group.Name = "Group";
+            Group.Padding = new Padding(0, 0, 0, 0);
+            Group.Size = new Size(478, 116);
+            Group.TabIndex = 10;
+            Group.TabStop = false;
+            Group.Text = "Select Flight";
+            Group.Enter += grpSave_Enter;
+            // 
+            // CombatFliteFlightSelector
+            // 
+            AutoScaleDimensions = new SizeF(8F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGoldenrod;
+            ClientSize = new Size(484, 118);
+            ControlBox = false;
+            Controls.Add(Group);
+            Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "CombatFliteFlightSelector";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "CombatFliteFlightSelector";
+            Load += CombatFliteFlightSelector_Load;
+            Group.ResumeLayout(false);
+            ResumeLayout(false);
+        }
 
-		}
+        #endregion
 
-		#endregion
-
-		private System.Windows.Forms.ComboBox comboBoxFlight;
-		private Base.Controls.DTCLabel dtcLabel1;
-		public Base.Controls.DTCButton btnCancel;
-		public Base.Controls.DTCButton btnOK;
-	}
+        private ComboBox comboBoxFlight;
+        public Base.Controls.DTCButton btnCancel;
+        public Base.Controls.DTCButton btnOK;
+        private Base.Controls.DTCGroupBox Group;
+    }
 }
--- dcs-dtc/UI/CommonPages/CombatFliteFlightSelector.cs
+++ dcs-dtc/UI/CommonPages/CombatFliteFlightSelector.cs
@@ -6,36 +6,46 @@ using System.Xml.XPath;
 
 namespace DTC.UI.CommonPages
 {
-	public partial class CombatFliteFlightSelector : Form
-	{
-		public string SelectedItem { get; private set; }
+    public partial class CombatFliteFlightSelector : Form
+    {
+        public string SelectedItem { get; private set; }
 
-		public CombatFliteFlightSelector(string file)
-		{
-			InitializeComponent();
+        public CombatFliteFlightSelector(string file)
+        {
+            InitializeComponent();
 
-			// Populate the combobox with the flight names
-			var doc = XDocument.Parse(file);
-			var flights = doc.XPathSelectElements("//Route").Select(r => r.Element("Name")?.Value);
-			comboBoxFlight.Items.AddRange(flights.ToArray<object>());
+            // Populate the combobox with the flight names
+            var doc = XDocument.Parse(file);
+            var flights = doc.XPathSelectElements("//Route").Select(r => r.Element("Name")?.Value);
+            comboBoxFlight.Items.AddRange(flights.ToArray<object>());
 
-			// Select the first flight
-			comboBoxFlight.SelectedIndex = 0;
-		}
+            // Select the first flight
+            comboBoxFlight.SelectedIndex = 0;
+        }
 
-		private void btnOK_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.OK;
-		}
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
 
-		private void btnCancel_Click(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.Cancel;
-		}
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
 
-		private void comboBoxFlight_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			SelectedItem = comboBoxFlight.SelectedItem.ToString();
-		}
-	}
+        private void comboBoxFlight_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedItem = comboBoxFlight.SelectedItem.ToString();
+        }
+
+        private void CombatFliteFlightSelector_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpSave_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
